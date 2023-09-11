4c1a470a4ab3a2702d26caa707c5402b0d1d9265
Da-vinia <daviniatosco@gmail.com>
2023-09-11 23:32:19 +0200

    create login route&view

--- routes/auth/login.routes.js
+++ routes/auth/login.routes.js
@@ -1,24 +1,32 @@
-//const express = require('express').Router();
+//const express = require('express');
 // const loginRouter = express.Router();
 const router = require('express').Router();
 const bcryptjs = require('bcryptjs');
 const User = require('../../models/User.model');
 
-router.get('/login', (req, res) => res.render('auth/login'));
+// router.get('/login', (req, res) => res.render('auth/login'));
+
+// router.get('/profile', (req, res) => {
+//     res.render('user/profile', { userInSession: req.session.currentUser });
+//   });
+
+router.get('/posts', (req, res) => {
+    res.render('posts', { userInSession: req.session.currentUser });
+});
 
 router.post('/login', (req, res, next) => {
     const { username, password } = req.body;
 
     if(username === '' || password === '') {
         res.render('auth/login', {
-            errorMessage: 'Surfer, please enter both, your username and password to login'
+            errorMessage: 'Surfer, please enter both, your username and password to login 👆'
         });
         return;
     }
     User.findOne({username})
         .then(user => {
             if(!user) {
-                re.render('auth/login', {
+                res.render('auth/login', {
                     errorMessage: 'User not found and/or incorrect password 🤖'
                 });
                 return;
@@ -26,18 +34,18 @@ router.post('/login', (req, res, next) => {
                 req.session.currentUser = user;
                 res.redirect('/posts');
             } else {
-                res.render('auth/login', {errorMessage: 'User not found and/or incorrect password 🤖'})
+                res.render('auth/login', {errorMessage: 'User not found and/or incorrect password 🤖'});
             }
         })
         .catch(error => next(error));
-})
+});
 
-const isAuthenticated = (req, res, next) => {
-    if(req.session.currentUser) {
-        next();
-    } else {
-        res.redirect('/login');
-    }
-};
+// const isAuthenticated = (req, res, next) => {
+//     if(req.session.currentUser) {
+//         next();
+//     } else {
+//         res.redirect('/login');
+//     }
+// };
 
 module.exports = router;
